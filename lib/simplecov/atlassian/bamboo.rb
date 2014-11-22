ed39bf32638819f369b71427c46a93c22b14322f
Mikko Kokkonen <mikko@owlforestry.com>
2014-11-23 00:25:25 +0800

    Initial commit

---
+++ lib/simplecov/atlassian/bamboo.rb
@@ -0,0 +1,9 @@
+require "simplecov/atlassian/bamboo/version"
+
+module Simplecov
+  module Atlassian
+    module Bamboo
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/simplecov/atlassian/bamboo/version.rb
@@ -0,0 +1,7 @@
+module Simplecov
+  module Atlassian
+    module Bamboo
+      VERSION = "0.0.1"
+    end
+  end
+end
